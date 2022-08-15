7b74f3bd0e608469de6e8bd343c3e6615425e738
Henry Anthony <henrymeiv363@gmail.com>
2022-08-15 19:58:16 +0800

    added meal components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <React.Fragment>
+      <Header/>
+      <main>
+        <Meals/>
+      </main>
+    </React.Fragment>
   );
 }
 
